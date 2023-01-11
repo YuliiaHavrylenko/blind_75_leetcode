1b69e10a94f1363b4043852babe87ba4b5f60fb1
Yuliia Havrylenko <volttabaki@gmail.com>
2023-01-12 00:04:35 +0400

    Added valid Palindrome

---
+++ Two Pointers/Valid Palindrome.py
@@ -0,0 +1,16 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+#
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.replace(" ", "")
+        for char in s:
+            s = s.replace(char, '').lower()
+        return s == s[::-1]
