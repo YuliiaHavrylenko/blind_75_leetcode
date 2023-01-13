cabedb9583f374d1f616d550508908d3c8195e0d
Yuliia Havrylenko <volttabaki@gmail.com>
2023-01-13 14:11:00 +0400

    Added Best Time to Buy Stocks

---
+++ Sliding Window/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,24 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+#
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+#
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+#
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_price = prices[-1]
+        for i in prices:
+            if i < min_price and prices.index(i) < prices.index(max_price):
+                min_price = i
+            elif i > max_price and prices.index(i) > prices.index(min_price):
+                max_price = i
+        if max_price > min_price:
+            return max_price - min_price
+        else:
+            return 0
