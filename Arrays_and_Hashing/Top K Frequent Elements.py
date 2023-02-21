1e38888a1256045c756c02e3a0f4fb05a9c67ed5
Yuliia Havrylenko <volttabaki@gmail.com>
2023-02-20 20:08:37 -0800

    Added Solution Top K Frequent Elements

--- Arrays_and_Hashing/Top K Frequent Elements.py
+++ Arrays_and_Hashing/Top K Frequent Elements.py
@@ -8,3 +8,22 @@
 #
 # Input: nums = [1], k = 1
 # Output: [1]
+
+# Bucket sort
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        for n,c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
